e78321bcf2d0d55308d74d756afb0f2c5e66a4ba
Martin Weiss <martin.clyde.weiss@gmail.com>
2021-09-23 15:27:52 -0400

    creates zipped datasets

--- create_dataset.py
+++ create_dataset.py
@@ -4,38 +4,37 @@ import torch
 import os
 import zipfile
 from tqdm import tqdm
-from PIL import Image
-import cv2
 import pickle
 import multiprocessing as mp
+from multiprocessing import Pool
+from PIL import Image, ImageFile
+from io import BytesIO
 
-num_episodes = 64 # 1024 # 2^10
+num_episodes = 1024 # 1024 # 2^10
 max_timesteps_per_episode = 200
 
-def writer(outbox, output_path, num_processes):
+def writer(outbox, output_path, num_todo):
     "single process that writes compressed files to disk"
     num_finished = 0
     with zipfile.ZipFile(output_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
-
-        while True:
+        while num_finished < num_todo:
             # all compression processes have finished
             if num_finished >= max_timesteps_per_episode * num_episodes: break
 
             tardata = outbox.get()
-
-            # a compression process has finished
-            if tardata == None:
+            print(f"{num_finished} / {num_todo}")
+            if any(tardata):
                 num_finished += 1
+            else:
                 continue
 
-            print(num_finished)
-            fn, data = tardata
-            retval, buf = cv2.imencode('.png',  data)
+            name, data = tardata
+            im = Image.fromarray(np.frombuffer(data, dtype=np.uint8).reshape(96, 96, 3))
+            image_file = BytesIO()
+            im.save(image_file, 'PNG')
 
-            name = os.path.join(output_path, fn) + '.zip'
-            zipf.writestr(name, buf)
+            zipf.writestr(name, image_file.getvalue())
 
-    return
 
 def gen_data(seed, outbox):
     np.random.seed(seed)
@@ -46,7 +45,7 @@ def gen_data(seed, outbox):
 
     start = seed * num_episodes // os.cpu_count()
     end = num_episodes // os.cpu_count() + seed * num_episodes // os.cpu_count()
-    for i in tqdm(range(start, end), desc="Episode number..."):
+    for i in range(start, end):
         obs = env.reset()
 
         for t in range(max_timesteps_per_episode+10):
@@ -54,23 +53,24 @@ def gen_data(seed, outbox):
                 continue
             action = get_action(obs)
             obs, rew, done, _ = env.step(action)
-            outbox.put(f"{i}-{t}.png", pickle.dump(obs))
+            outbox.put([f"{i}-{t}.png", obs.tobytes()])
+    print(f"gen_data {seed} finished")
+    return
 
 if __name__ == '__main__':
-    # num_processes = os.cpu_count()
-    num_processes = 1
+    num_processes = 8 # multiple of 2, ideally
+    num_todo = num_episodes * max_timesteps_per_episode
 
     outbox = mp.Queue(4*num_processes)  # limit size
 
-    # n processes to compress
     simulators = [mp.Process(target=gen_data, args=(seed, outbox)) for seed in range(num_processes)]
     for s in simulators: s.start()
 
     # one process to write
-    writer = mp.Process(target=writer, args=(outbox, fld, num_processes))
+    output_path = "vae_dataset.zip"
+    writer = mp.Process(target=writer, args=(outbox, output_path, num_todo))
     writer.start()
     writer.join()  # wait for it to finish
+    for s in simulators: s.join()
     print('done!')
 
-    with Pool(num_processes) as p:
-        p.map(gen_data, range(num_processes))
